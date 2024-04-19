930b0a1570cf657657588eeef3f626787bf4bece
DaviPedriVieira <davipedrivieira@gmail.com>
2024-04-19 16:54:49 -0300

    commit

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,22 @@ import { AppModule } from './app/app.module';
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
 
+  // loadTheme()
   const body: any = document.querySelector('body');
   const toggle: any = document.getElementById('toggle');
   toggle.onclick = function() {
     toggle.classList.toggle('active')
     body.classList.toggle('dark-mode')
+    // saveTheme()
   }
+
+  // function saveTheme() {
+  //   localStorage.setItem('Theme', toggle.classList)
+  // }
+
+  // function loadTheme() {
+  //   let theme = localStorage.getItem('Theme')
+  //   if(theme == 'Active') {
+  //     toggle.click()
+  //   }
+  // }
