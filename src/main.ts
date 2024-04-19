0dd20f7577b3eac638a068317a55452bb70db252
DaviPedriVieira <davipedrivieira@gmail.com>
2024-04-19 16:52:19 -0300

    botão theme

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,10 @@ import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+  const body: any = document.querySelector('body');
+  const toggle: any = document.getElementById('toggle');
+  toggle.onclick = function() {
+    toggle.classList.toggle('active')
+    body.classList.toggle('dark-mode')
+  }
