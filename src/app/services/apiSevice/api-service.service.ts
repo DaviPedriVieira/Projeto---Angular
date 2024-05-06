997cfa6d9d86f7efd90ffb185886922c9c337d6a
DaviPedriVieira <davipedrivieira@gmail.com>
2024-05-06 14:06:58 -0300

    end

--- src/app/services/apiSevice/api-service.service.ts
+++ src/app/services/apiSevice/api-service.service.ts
@@ -23,7 +23,7 @@ export class ApiServiceService {
 
   fetchWeatherData(): Observable<IWeatherData> {
     let selectedCity = localStorage.getItem('Cidade');
-    let apiLink = `https://api.hgbrasil.com/weather?format=json-cors&key=bac82211	&city_name=${selectedCity}`;
+    let apiLink = `https://api.hgbrasil.com/weather?format=json-cors&key=bac82211&city_name=${selectedCity}`;
     return this.http.get<IWeatherData>(apiLink).pipe( 
       tap((weatherData) => { 
         this.setBehaviorWeatherData(weatherData);
